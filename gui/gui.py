eadece57bb7790d755174342b23a75ebca1b82d1
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-03-19 21:37:01 +0100

    fixed missing parameter in build

--- gui/gui.py
+++ gui/gui.py
@@ -7,8 +7,9 @@ def expose_scrape(username, password, imap_server):
     domains = WhereDoIHaveAnAccount.scraper.scrape(username, password, imap_server)
     return list(domains)
 
+
 def main():
     # Set web files folder
     eel.init('gui/web')
-    eel.start(page='index.html')
+    eel.start(page='index.html', port=8080)
 
