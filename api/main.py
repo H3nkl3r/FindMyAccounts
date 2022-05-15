a4c3c931f13196faaeec8e1c90e2876350a0412b
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-05-16 00:30:29 +0200

    trying to fix cors error

--- api/main.py
+++ api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from WhereDoIHaveAnAccount.scraper import scrape
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:63342",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # get list of accounts from a user
 # take email, password and imap-server as input
