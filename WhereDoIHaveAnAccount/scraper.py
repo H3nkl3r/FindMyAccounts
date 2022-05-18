5c53825ac8614c4e702ab1a2dbf734ed19ea8679
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-05-18 10:29:02 +0200

    added comments

--- WhereDoIHaveAnAccount/scraper.py
+++ WhereDoIHaveAnAccount/scraper.py
@@ -19,28 +19,37 @@ from imap_tools.errors import MailboxLoginError
 
 
 def get_domain_from_email(email):
+    """
+    Get the domain from an email address
+    :param email:
+    :return:
+    """
     return email[email.index('@') + 1:]
 
 
 def scrape(username, password, imap_server):
     """
+    Scrape the mailbox of a user
     :param username: email address
     :param password: password for the specified email address
     :param imap_server: imap server for specified email address
     :return: Set of domains
-
     """
     von = []
     try:
         with MailBox(imap_server).login(username, password) as mailbox:
+            # iterate over all folders
             for folder in MailBoxFolderManager(mailbox).list():
+                # not scraping the sent folder because it is not necessary
                 if 'sent' in folder.name.lower():
                     continue
                 mailbox.folder.set(folder.name)
                 found_nums = mailbox.numbers('ALL')
                 page_len = 100
+                # if needed because of even and odd folder sizes
                 pages = int(len(found_nums) // page_len) + 1 if len(found_nums) % page_len else int(
                     len(found_nums) // page_len)
+                # iterate over all pages
                 for page in range(pages):
                     page_limit = slice(page * page_len, page * page_len + page_len)
                     for msg in mailbox.fetch(headers_only=True, bulk=True, limit=page_limit):
@@ -52,6 +61,7 @@ def scrape(username, password, imap_server):
         logging.error(f"An exception of type {type(e).__name__}: {e}")
         return "An internal error has occurred!"
 
+    # get domains from email addresses
     domains = []
     for email_address in von:
         try:
@@ -59,6 +69,7 @@ def scrape(username, password, imap_server):
         except IndexError:
             continue
 
+    # Remove duplicates
     domains = list(set(domains))
     return domains
 
