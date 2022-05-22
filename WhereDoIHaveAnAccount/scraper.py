c63310fd789f46ca9740e9082bef1a4fbad9a8bd
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-05-22 12:31:18 +0200

    Ai based prediction (#9)
    * introduced an ai based prediction of potential accounts with sklearn
    * add function to filter domains with feather list
    * renamed existing functions

--- WhereDoIHaveAnAccount/scraper.py
+++ WhereDoIHaveAnAccount/scraper.py
@@ -1,12 +1,15 @@
 # Copyright (c) 2022 Timo Kühne
 import logging
+import math
 
 from imap_tools import MailBox, MailBoxFolderManager
 from imap_tools.errors import MailboxLoginError
 
 from email_validator import validate_email, EmailNotValidError
-
 import pandas as pd
+import pickle
+import nltk
+import re
 
 
 def get_domain_from_email(email):
@@ -15,18 +18,21 @@ def get_domain_from_email(email):
     :param email:
     :return:
     """
-    return email[email.index('@') + 1:]
+    try:
+        domain = email[email.index('@') + 1:]
+    except ValueError:
+        domain = None
+    return domain
 
 
-def scrape(username, password, imap_server):
+def get_email_headers(username, password, imap_server):
     """
     Scrape the mailbox of a user
     :param username: email address
     :param password: password for the specified email address
     :param imap_server: imap server for specified email address
-    :return: Set of domains
+    :return: Dataframe with subject and from_
     """
-
     # validate email address
     email = None
     try:
@@ -37,7 +43,7 @@ def scrape(username, password, imap_server):
         logging.error(f"An exception of type {type(e).__name__}: {e}")
         return "email is not valid"
 
-    von = []
+    email_header_df = pd.DataFrame(columns=['from', 'subject'])
     try:
         with MailBox(imap_server).login(email, password) as mailbox:
             # iterate over all folders
@@ -55,7 +61,7 @@ def scrape(username, password, imap_server):
                 for page in range(pages):
                     page_limit = slice(page * page_len, page * page_len + page_len)
                     for msg in mailbox.fetch(headers_only=True, bulk=True, limit=page_limit):
-                        von.append(msg.from_)
+                        email_header_df = email_header_df.append([{'from': msg.from_, 'subject': msg.subject}])
     except MailboxLoginError:
         return 'Authentication failed: If you have 2-Factor-Authentication activated for your Email, you need to use ' \
                'an App-Password '
@@ -63,19 +69,27 @@ def scrape(username, password, imap_server):
         logging.error(f"An exception of type {type(e).__name__}: {e}")
         return "An internal error has occurred!"
 
-    # get domains from email addresses
-    domains = []
-    for email_address in von:
-        try:
-            domains.append(get_domain_from_email(email_address))
-        except IndexError:
-            continue
-        except ValueError:
-            continue
+    email_header_df['domain'] = email_header_df['from'].apply(lambda x: get_domain_from_email(x))
 
-    # Remove duplicates
-    domains = list(set(domains))
-    return domains
+    # set type of columns to string
+    email_header_df['from'] = email_header_df['from'].astype(str)
+    email_header_df['subject'] = email_header_df['subject'].astype(str)
+
+    return email_header_df
+
+
+def distinct_scrape(username, password, imap_server):
+    """
+    Scrape the mailbox of a user
+    :param username: email address
+    :param password: password for the specified email address
+    :param imap_server: imap server for specified email address
+    :return: Set of domains
+    """
+
+    email_header_df = get_email_headers(username, password, imap_server)
+
+    return email_header_df['domain'].to_frame().drop_duplicates(subset=['domain'], ignore_index=True).to_dict(orient='records')
 
 
 def filtered_scrape(username, password, imap_server, filter_domains_list):
@@ -87,8 +101,79 @@ def filtered_scrape(username, password, imap_server, filter_domains_list):
     :param imap_server: imap server for specified email address
     :return: Set of domains
     """
-    domains = scrape(username, password, imap_server)
+    email_header_df = get_email_headers(username, password, imap_server)
     filter_list = pd.read_feather(filter_domains_list)
     filter_domains = filter_list['domain'].tolist()
-    return [domain for domain in domains if domain in filter_domains]
+    return email_header_df[email_header_df['domain'].isin(filter_domains)][:, 'domain'].drop_duplicates(subset=['domain'], ignore_index=True).to_dict(orient='records')
+
+
+def preprocess_data(email_header_df):
+    """
+    Preprocess the data
+    :param email_header_df: dataframe containing the email headers
+    :return: dataframe containing the email headers
+    """
+    # to lower case
+    email_header_df['subject'] = email_header_df['subject'].str.lower()
+
+    # remove all non-ascii characters
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: re.sub("[^a-z\u00C0-\u017F]", " ", x))
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: re.sub("ä", "ae", x))
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: re.sub("ö", "oe", x))
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: re.sub("ü", "ue", x))
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: re.sub("ß", "ss", x))
+
+    # tokenize
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: nltk.word_tokenize(x))
+
+    # remove stopwords
+    nltk.download('stopwords')
+    stopwords = nltk.corpus.stopwords.words('english') + nltk.corpus.stopwords.words('german')
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: [word for word in x if word not in stopwords])
+
+    # lemmatize
+    lemmatizer = nltk.stem.WordNetLemmatizer()
+    nltk.download('wordnet')
+    email_header_df['subject'] = email_header_df['subject'].apply(lambda x: [lemmatizer.lemmatize(word) for word in x])
+
+    return email_header_df
+
+
+def truncate(f, n):
+    return math.floor(f * 10 ** n) / 10 ** n
+
+
+def sklearn_scrape(username, password, imap_server, model, vector_model):
+    """
+    Scrape the mailbox of a user but only for domains that are in the filter_domains_list
+    :param vector_model: path to a pickle file which contains the vectorizer
+    :param model: path to a pickle file which contains a fitted sklearn model
+    :param username: email address
+    :param password: password for the specified email address
+    :param imap_server: imap server for specified email address
+    :return: Set of domains
+    """
+    email_header_df = get_email_headers(username, password, imap_server)
+
+    # load models
+    model = pickle.load(open(model, 'rb'))
+    vectorizer = pickle.load(open(vector_model, 'rb'))
+
+    # transform data
+    email_header_df = preprocess_data(email_header_df)
+
+    subject_features = vectorizer.transform([' '.join(o) for o in email_header_df['subject'].tolist()])
+
+    email_header_df['prediction'] = model.predict(subject_features.toarray())
+    email_header_df['probability'] = model.predict_proba(subject_features.toarray())[:, 1]
+
+    # cut to 4 decimals
+    email_header_df['probability'] = email_header_df['probability'].apply(lambda x: truncate(x, 2))
+
+    return email_header_df.loc[email_header_df['prediction'] == 1, ['domain', 'probability']].sort_values(by='probability', ascending=False).drop_duplicates(subset=['domain'], ignore_index=True).to_dict(orient='records')
+
+
+
+
+
 
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022 Timo Kühne
 import os
 
-from WhereDoIHaveAnAccount.scraper import scrape
+from WhereDoIHaveAnAccount.scraper import distinct_scrape
 
 TEST_EMAIl_USERNAME = os.environ['TEST_EMAIL_USERNAME']
 TEST_EMAIL_PASSWORD = os.environ['TEST_EMAIL_PASSWORD']
@@ -9,6 +9,6 @@ TEST_EMAIL_IMAP_SERVER = os.environ['TEST_EMAIL_IMAP_SERVER']
 
 
 def test_scrape():
-    domains = scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER)
+    domains = distinct_scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER)
     assert isinstance(domains, list)
     assert len(domains) is not 0
