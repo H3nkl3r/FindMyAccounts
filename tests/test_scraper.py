b2f37adb02a6c0789e3197cff3c5a6f2825e7a2e
Timo Kühne <22354443+H3nkl3r@users.noreply.github.com>
2022-06-01 22:31:54 +0200

    added missing tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -3,7 +3,8 @@ import os
 import sys
 import pytest
 
-from FindMyAccounts.scraper import distinct_scrape, get_email_headers, sklearn_scrape, filtered_scrape
+from FindMyAccounts.scraper import distinct_scrape, get_email_headers, sklearn_scrape, filtered_scrape, \
+    get_domain_from_email
 
 TEST_EMAIl_USERNAME = os.environ['TEST_EMAIL_USERNAME']
 TEST_EMAIL_PASSWORD = os.environ['TEST_EMAIL_PASSWORD']
@@ -30,27 +31,42 @@ def test_get_email_header_login_error():
 @pytest.mark.skipif(sys.version_info <= (3, 10), reason='the generated pickle files are very sensitive to different '
                                                         'versions of sklearn with which the model was exported')
 def test_sklearn_scrape():
-    domains = sklearn_scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER, 'tests/dump_model.pkl', 'tests/dump_vectorizer.pkl')
+    domains = sklearn_scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER, 'tests/dump_model.pkl',
+                             'tests/dump_vectorizer.pkl')
     assert isinstance(domains, list)
     assert len(domains) is not 0
 
 
 def test_filtered_scrape():
-    domains = filtered_scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER, 'tests/dump_filter_list.feather')
+    domains = filtered_scrape(TEST_EMAIl_USERNAME, TEST_EMAIL_PASSWORD, TEST_EMAIL_IMAP_SERVER,
+                              'tests/dump_filter_list.feather')
     assert isinstance(domains, list)
     assert len(domains) is not 0
 
 
 def test_filtered_scrape_error():
-    domains = filtered_scrape('TEST_EMAIl_USERNAME', 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER', 'tests/dump_filter_list_error.feather')
+    domains = filtered_scrape('TEST_EMAIl_USERNAME', 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER',
+                              'tests/dump_filter_list_error.feather')
     assert isinstance(domains, str)
 
 
 def test_sklearn_scrape_error():
-    domains = sklearn_scrape('TEST_EMAIl_USERNAME', 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER', 'tests/dump_model_error.pkl', 'tests/dump_vectorizer_error.pkl')
+    domains = sklearn_scrape('TEST_EMAIl_USERNAME', 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER',
+                             'tests/dump_model_error.pkl', 'tests/dump_vectorizer_error.pkl')
     assert isinstance(domains, str)
 
 
 def test_distinct_scrape_error():
     domains = distinct_scrape('TEST_EMAIl_USERNAME', 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER')
     assert isinstance(domains, str)
+
+
+def test_domain_from_email_value_error():
+    domain = get_domain_from_email('')
+    assert domain is None
+
+
+def test_get_email_header_error():
+    response = get_email_headers(TEST_EMAIl_USERNAME, 'TEST_EMAIL_PASSWORD', 'TEST_EMAIL_IMAP_SERVER')
+    assert response == 'An internal error has occurred!'
+
